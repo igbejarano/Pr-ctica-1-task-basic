7f5503074ddd17c744b6ce9819f11650dc30cd4d
igbejarano <bejaranogissel21@gmail.com>
2022-02-04 19:07:25 -0300

    terminado

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setTasks([...tasks, task]);
+  };
+
+  return (
+    <div className="container">
+      <h1>Task Basic</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="Ingrese la tarea"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button type="submit">Agregar Tarea</button>
+      </form>
+      <div className="tasks-container">
+        {tasks.map((item, index) => (
+          <div key={index} className="task-card">
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
